7852e4411ff2311cfb1869886c4a1a127549275a
Richard Giddings <rmpgiddings@gmail.com>
2026-04-17 09:15:09 +0100

    Add python-decouple to manage config

--- server.py
+++ server.py
@@ -5,15 +5,18 @@ from flask import (
     Response,
     request,
 )
+from decouple import config
+
 
 app = Flask(__name__)
-app.secret_key = 'asdf'
+app.secret_key = config('SECRET_KEY')
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 @app.route('/create', methods=['POST'])
 def create_channel():
     pass
+
 
 @app.route('/post', methods=['POST'])
 def post():
@@ -25,15 +28,18 @@ def post():
     r.publish(channel, '[%s][%s]: %s' % (now.isoformat(), user, message))
     return Response(status=204)
 
+
 def event_stream():
     pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe('chat')
     for message in pubsub.listen():
         yield f"{message['data']}\n\n"
 
+
 @app.route('/stream')
 def stream():
     return Response(event_stream(), mimetype="text/event-stream")
+
 
 if __name__ == "__main__":
     app.run()
