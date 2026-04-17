62867457cfe4cae1dfc2702841e8ecf87405f95c
Richard Giddings <rmpgiddings@gmail.com>
2026-04-17 10:05:10 +0100

    Channel name now passed to streaming so we can have more channels

--- server.py
+++ server.py
@@ -13,11 +13,6 @@ app.secret_key = config('SECRET_KEY')
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
-@app.route('/create', methods=['POST'])
-def create_channel():
-    pass
-
-
 @app.route('/post', methods=['POST'])
 def post():
     message = request.form['message']
@@ -29,16 +24,17 @@ def post():
     return Response(status=204)
 
 
-def event_stream():
+def event_stream(channel):
     pubsub = r.pubsub(ignore_subscribe_messages=True)
-    pubsub.subscribe('chat')
+    pubsub.subscribe(channel)
     for message in pubsub.listen():
         yield f"{message['data']}\n\n"
 
 
 @app.route('/stream')
 def stream():
-    return Response(event_stream(), mimetype="text/event-stream")
+    channel = request.args.get('channel')
+    return Response(event_stream(channel), mimetype="text/event-stream")
 
 
 if __name__ == "__main__":
